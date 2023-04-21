672ae29ce047ae53cd3c37108b8e4ec46d6a98cb
Deepak Louis <deepak.l@zohocorp.com>
2023-04-21 19:39:15 +0530

    hidden message

---
+++ index.js
@@ -0,0 +1,12 @@
+function showAlert() {
+    alert("கண்கள் திறக்கும் வரை தான் கனவு நீடிக்கும்..ஆனால் என் கண்கள் மூடும் வரை என் காதல் நீடிக்கும்.\n\n Mayirula! Etho konchondu i love u. Avlodan(dot)!!")
+}
+
+function showPopup() {
+    document.getElementById('hidden').style.display = 'block';
+}
+
+// Function to hide popup
+function hidePopup() {
+    document.getElementById('hidden').style.display = 'none';
+}
